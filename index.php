d5045b2d6d4506222894dfe8eef110133cb529a2
Zac Laudick <zlaudick@nmsu.edu>
2016-09-30 15:48:42 -0600

    working on nav

--- index.php
+++ index.php
@@ -42,7 +42,7 @@
 
 					<!-- begin navbar -->
 					<nav class="navbar navbar-inverse navbar-fixed-top">
-						<!-- Brand and toggle get grouped for better mobile display -->
+						<div class="container">
 						<div class="navbar-header">
 							<button type="button" class="navbar-toggle collapsed" data-toggle="collapse"
 									  data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
@@ -58,20 +58,9 @@
 						<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
 							<ul class="nav navbar-nav navbar-right">
 								<li><a href="#">FAQs</a></li>
-								<li class="dropdown">
-									<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true"
-										aria-expanded="false">Moar Links <span class="caret"></span></a>
-									<ul class="dropdown-menu">
-										<li><a href="#">Action</a></li>
-										<li><a href="#">Another action</a></li>
-										<li><a href="#">Something else here</a></li>
-										<li role="separator" class="divider"></li>
-										<li><a href="#">Separated link</a></li>
-									</ul>
-								</li>
 							</ul>
 						</div><!-- /.navbar-collapse -->
-
+						</div>
 					</nav>
 				</div>
 
